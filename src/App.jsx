7838ac63ff5e404ae22061107ca2d1dbbc93a55a
capt-ines <ines.swieczkowska02@gmail.com>
2024-11-14 10:56:54 +0100

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <div>☕️</div>;
+  return (
+    <div>
+      <h1>Counters</h1>
+      <MyButton></MyButton>
+      <MyButton></MyButton>
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
 export default App;
