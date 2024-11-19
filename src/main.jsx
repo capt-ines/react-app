e0d2fbe219fab88be75040a21143d3582bbd192c
capt-ines <ines.swieczkowska02@gmail.com>
2024-11-19 17:01:14 +0100

    added speed counter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ListButton from "./ListButton";
+import Counter from "./Counter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ListButton />
+    <Counter></Counter>
   </StrictMode>
 );
